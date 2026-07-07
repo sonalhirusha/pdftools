a1e1024f7ac54dc484405de3a19002fbdd1c0714
zeroclothing23 <dev@zeroclothing23.com>
2026-07-07 11:54:24 +0530

    add third ad network script

--- src/components/layout/Analytics.tsx
+++ src/components/layout/Analytics.tsx
@@ -14,6 +14,7 @@ export function Analytics() {
   useEffect(() => {
     ;['https://pl30241180.effectivecpmnetwork.com/be/b6/c1/beb6c10bacd295422228e7015129b14e.js',
       'https://pl30241182.effectivecpmnetwork.com/dc/6d/ca/dc6dca26e73c51823850fab290f2b0c1.js',
+      'https://www.effectivecpmnetwork.com/iubme16h3v?key=a190b566f3e6a174dff077c12f5d3e57',
     ].forEach(src => {
       if (document.querySelector(`script[src="${src}"]`)) return
       const s = document.createElement('script')
